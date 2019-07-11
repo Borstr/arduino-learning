17a4030eac9991df0d296d3dcd3117c6638647e7
Borys <Borys@DESKTOP-U977PG2>
2019-07-11 22:14:50 +0200

    initial commit

--- app.js
+++ app.js
@@ -2,34 +2,13 @@ const five = require('johnny-five');
 const board = new five.Board();
 
 board.on('ready', () => {
-  const btn1 = new five.Button(2);
-  const btn2 = new five.Button(7);
-  const btn3 = new five.Button(8);
-  const led = new five.Led(12);
+  const servo = five.Servo(10);
+  const btn1 = five.Button(2);
+  const btn2 = five.Button(7);
 
-  const rand = randomNumber(1, 3);
+  let degree = 0;
+  servo.to(0);
 
-  flashXTimes(rand, led, 250);
-
-  btn1.on('down', () => checkIfPressedCorrectBtn(1, rand, led));
-  btn2.on('down', () => checkIfPressedCorrectBtn(2, rand, led));
-  btn3.on('down', () => checkIfPressedCorrectBtn(3, rand, led));
+  btn1.on('hold', () => (degree != 0) && servo.to(--degree));
+  btn2.on('hold', () => (degree != 180) && servo.to(++degree));
 });
-
-function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-const checkIfPressedCorrectBtn = (index, number, led) => (index === number) && flashXTimes(3, led, 100);
-
-function flashXTimes(number, led, time) {
-  let current = 1;
-  
-  const interval = setInterval(() => {
-    if(current % 2 == 1) led.on();
-    else if(current % 2 == 0) led.off();
-
-    if(current / 2 == number) clearInterval(interval);
-    current++;
-  }, time);
-}
