907ca7b05e1142208f2e747a8a1992b25626e8c4
Borys <Borys@DESKTOP-U977PG2>
2019-07-12 23:23:12 +0200

    assigned all elements to variables

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const five = require('johnny-five');
 const board = new five.Board();
 
 board.on('ready', () => {
-  const digit = new five.Led.Digits({
-    pins: {
-      data: 2,
-      clock: 3,
-      cs: 4
-    }
-  });
+  const leds = [
+    new five.Led(3),
+    new five.Led(5),
+    new five.Led(7),
+    new five.Led(9),
+  ];
 
-  digit.print(1)
+  const btns = [
+    new five.Button(2),
+    new five.Button(4),
+    new five.Button(6),
+    new five.Button(8),
+  ];
 });
