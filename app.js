70f262f84ecb4cae14b34b22bb880fd55e67dc99
Borys <Borys@DESKTOP-U977PG2>
2019-07-12 23:15:35 +0200

    failed try, gonna come back soon

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const five = require('johnny-five');
 const board = new five.Board();
 
 board.on('ready', () => {
-  const servo = five.Servo(10);
-  const btn1 = five.Button(2);
-  const btn2 = five.Button(7);
+  const digit = new five.Led.Digits({
+    pins: {
+      data: 2,
+      clock: 3,
+      cs: 4
+    }
+  });
 
-  let degree = 0;
-  servo.to(0);
-
-  btn1.on('hold', () => (degree != 0) && servo.to(--degree));
-  btn2.on('hold', () => (degree != 180) && servo.to(++degree));
+  digit.print(1)
 });
