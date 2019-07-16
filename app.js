36470f26707127dc40cedda860061e83d3d6431f
Borys <Borys@DESKTOP-U977PG2>
2019-07-16 02:27:14 +0200

    lighting up leds via the website

--- app.js
+++ app.js
@@ -9,4 +9,35 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('pages/index'));
 
+board.on('ready', () => {
+  const ledState = {
+    red: false,
+    blue: false,
+    green: false,
+    yellow: false
+  }
+  
+  const ledRed = five.Led(7);
+  const ledBlue = five.Led(6);
+  const ledYellow = five.Led(5);
+  const ledGreen = five.Led(8);
+  
+  app.get('/led/:type', (req, res) => {
+    console.log(req.params.type)
+    switch(req.params.type) {
+      case 'blue': switchLed(ledBlue, ledState, 'blue'); res.send('ok'); break;
+      case 'red': switchLed(ledRed, ledState, 'red'); res.send('ok'); break;
+      case 'green': switchLed(ledGreen, ledState, 'green'); res.send('ok'); break;
+      case 'yellow': switchLed(ledYellow, ledState, 'yellow'); res.send('ok'); break;
+    }
+  });
+});
+
+const switchLed = (led, state, field) => {
+  if(state[field]) led.off();
+  else led.on();
+
+  state[field] = !state[field];
+}
+
 app.listen(8080, () => console.log('Server Listening on port 8080.'));
