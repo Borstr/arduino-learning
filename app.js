b957d2ea81a08419ca5596384d6917ad152311f4
Borys <Borys@DESKTOP-U977PG2>
2019-07-19 09:54:33 +0200

    first try on controlling servos with joystick

--- app.js
+++ app.js
@@ -3,102 +3,36 @@ const five = require("johnny-five");
 const board = new five.Board();
 
 board.on('ready', () => {
-  let isStarted = false;
-  let timings = [];
-  let start;
+  const joystick = new five.Joystick({
+    pins: [
+      'A0',
+      'A1'
+    ]
+  });
 
-  const startBtn = new five.Button(7);
-  const startLed = new five.Led(8);
+  const servo = new five.Servo(10);
+
+  servo.center();
   
-  const leds = [
-    new five.Led(10),
-    new five.Led(11),
-    new five.Led(12),
-    new five.Led(13)
-  ];
-
-  const timingBtns = [
-    new five.Button(2),
-    new five.Button(3),
-    new five.Button(4),
-    new five.Button(5)
-  ];
-
-  timingBtns.map((btn, index) => btn.on('down', () => handleTimingButtonDown(index)));
-  timingBtns.map((btn, index) => btn.on('up', () => handleTimingButtonUp(index)));
-
-  startBtn.on('down', handleStartBtnDown);
+  let servoAngle = 0;
   
-  function handleStartBtnDown() {
-    if(!isStarted) {
-      startLed.on();
-    } else {
-      startLed.off();
-      recreate();
-    }
+  joystick.on('change', handleJoystickChange);
 
-    isStarted = !isStarted;
-  }
-
-  function handleTimingButtonDown(index) {
-    if(isStarted) {
-      if(timings[timings.length - 1] && timings[timings.length - 1].index !== 'break' && start) {
-        leds[index].on();
-
-        timings.push({
-          index: 'break',
-          time: new Date() - start
-        });
-
-        start = new Date();
-      } else {
-        start = new Date();
-        leds[index].on();
+  function handleJoystickChange() {
+    if(this.x > .1) {
+      if(servoAngle < 180) {
+        servoAngle += 180 / 50;
+      }
+    } else if(this.x < -.1) {
+      if(servoAngle > 0) {
+        servoAngle -= 180 / 50;
       }
     }
-  }
 
-  function handleTimingButtonUp(index) {
-    if(isStarted) {
-      const currTime = new Date();
-      
-      timings.push({ 
-        index,
-        time: currTime - start
-      });
-  
-      start = currTime;
-      leds[index].off();
-    }
-  }
+    if(servoAngle > 180) servoAngle = 180;
+    if(servoAngle < 0) servoAngle = 0;
 
-  function recreate() {    
-    try {
-      const { index, time } = timings[0];
-      
-      if(index != 'break') {
-        leds[index].on();
-      
-        setTimeout(() => {
-          leds[index].off();
-          timings.shift();
-          recreate();
-        }, time);
-      } else {
-        setTimeout(() => {
-          timings.shift();
-          recreate();
-        }, time);
-      }
-    } catch {
-      for(let i = 0; i < 3; i++) {
-        const j = i;
-        
-        setTimeout(() => {
-          startLed.on();
-          setTimeout(() => startLed.off(), 250 * j + 125);
-        }, 250 * j);
-      }
-    }
+    console.log(this.x, servoAngle)
+    servo.to(servoAngle);
   }
 });
