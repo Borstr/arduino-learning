f1e046b022e4ff06890bf151fc8ea88f45d1a83f
Borys <Borys@DESKTOP-U977PG2>
2019-08-12 15:48:38 +0200

    finished servo logic

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ const {
 } = require('./codes');
 
 board.on('ready', function() {
+  const controls = {
+    servoControl: 'joint',
+    isMemorizing: false,
+    isReplaying: false,
+    isControllable: true
+  }
+  const degrees = {
+    ground: 0,
+    middle: 90
+  }
+  
   const {
     btns,
     led,
@@ -18,37 +29,94 @@ board.on('ready', function() {
     joystick
   } = devices(five);
 
+  // Handling servos with joystick
+  const handleJoystickChange = (
+    middleJointServo, 
+    groundJointServo, 
+    middleRotatorServo,
+    x,
+    y
+  ) => {
+    const rotate180DegServo = () => {
+      if(x > .8 && degrees.middle <= 160) {
+        degrees.middle = degrees.middle + 1;
+        middleJointServo.to(degrees.middle);
+      }
+      if(x < -.8 && degrees.middle >= 90) {
+        degrees.middle = degrees.middle - 1;
+        middleJointServo.to(degrees.middle);
+      }
+      if(y > .8 && degrees.ground <= 100) {
+        degrees.ground = degrees.ground + 1;
+        groundJointServo.to(degrees.ground);
+      }
+      if(y < -.8 && degrees.ground >= 0) {
+        degrees.ground = degrees.ground - 1;
+        groundJointServo.to(degrees.ground);
+      }
+    }
+    
+    const rotate360DegServo = () => {
+      if(x > .2) middleRotatorServo.cw(x);
 
+      if(x < -.2) middleRotatorServo.ccw(-x);
+
+      if(x < .1 && x > -.1) middleRotatorServo.stop();
+    }
+    
+    if(controls.servoControl === 'joint') rotate180DegServo();
+    else rotate360DegServo();
+  }
+
+  joystick.on('data', function() {
+    handleJoystickChange(
+      middleJointServo,
+      groundJointServo,
+      middleRotatorServo,
+      this.x,
+      this.y
+    );
+  });
+
+  btns[0].on('down', () => switchToJointServos(controls));
+  btns[1].on('down', () => switchToRotatorServos(controls));
+  btns[2].on('down', () => switchMemorizingMovement(controls));
+  btns[3].on('down', () => switchRecreatingMovement(controls));
 });
 
-// Handling servos with joystick
-function handleJoystickChange() {
-  
-}
-
-function rotate180DegServo() {
-
-}
-
-function rotate360DegServo() {
-
-}
-
 //Buttons
-function switchToJointServos() {
-  
+function switchToJointServos(controls) {
+  if(controls.isControllable) {
+    controls.servoControl = 'joint';
+    console.log('joint')
+  }
 }
 
-function switchToRotatorServos() {
-
+function switchToRotatorServos(controls) {
+  if(controls.isControllable) {
+    controls.servoControl = 'rotator';
+    console.log('rotator')
+  }
 }
 
-function switchMemorizeMovement() {
-
+function switchMemorizingMovement(controls) {
+  if(controls.isControllable) {
+    controls.isMemorizing = true;
+    controls.isReplaying = false;
+    controls.isControllable = true;
+  }
 }
 
-function recreateMovement() {
-
+function switchRecreatingMovement(controls) {
+  if(!controls.isReplaying) {
+    controls.isMemorizing = false;
+    controls.isReplaying = true;
+    controls.isControllable = false;
+  } else {
+    controls.isMemorizing = false;
+    controls.isReplaying = false;
+    controls.isControllable = true;
+  }
 }
 
 // LED
