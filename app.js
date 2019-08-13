93f17ad227cadecb120c26269e07c5253a0317f0
Borys <Borys@DESKTOP-U977PG2>
2019-08-13 14:57:12 +0200

    finished mechanism responsible for recreating the memorized movements

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const five = require("johnny-five");
 const board = new five.Board();
 const devices = require('./devices');
-const {
-  memorizing,
-  replaying,
-  jointServos,
-  rotatorServos
-} = require('./codes');
+
+const SERVO_DEGREE_TIME = .1;
+let middleRotatorStart, middleRotatorEnd;
 
 board.on('ready', function() {
   const controls = {
@@ -44,6 +41,7 @@ board.on('ready', function() {
 
   joystick.on('data', function() {
     handleJoystickChange(
+      controls,
       middleJointServo,
       groundJointServo,
       middleRotatorServo,
@@ -55,13 +53,30 @@ board.on('ready', function() {
   });
 
   btns[0].on('down', () => switchToJointServos(controls));
+
   btns[1].on('down', () => switchToRotatorServos(controls));
-  btns[2].on('down', () => switchToMemorizingMovement(controls, led, servoDegrees, servosMemory));
-  btns[3].on('down', () => switchToRecreatingMovement(controls, led));
+
+  btns[2].on('down', () => switchToMemorizingMovement(
+    servosMemory,
+    controls, 
+    led, 
+    servoDegrees, 
+    servoTurns
+  ));
+
+  btns[3].on('down', () => switchToRecreatingMovement(
+    controls, 
+    led,
+    servosMemory,
+    middleJointServo,
+    groundJointServo,
+    middleRotatorServo
+  ));
 });
 
 // Handling servos with joystick
 function handleJoystickChange(
+  controls,
   middleJointServo, 
   groundJointServo, 
   middleRotatorServo,
@@ -76,43 +91,49 @@ function handleJoystickChange(
   function rotate180DegServo() {
     if(x > .8 && servoDegrees.middle <= 160) {
       servoDegrees.middle = servoDegrees.middle + 1;
-      middleJointServo.to(servoDegrees.middle, .1);
+      middleJointServo.to(servoDegrees.middle, SERVO_DEGREE_TIME);
     }
 
     if(x < -.8 && servoDegrees.middle >= 90) {
       servoDegrees.middle = servoDegrees.middle - 1;
-      middleJointServo.to(servoDegrees.middle, .1);
+      middleJointServo.to(servoDegrees.middle, SERVO_DEGREE_TIME);
     }
 
     if(y > .8 && servoDegrees.ground <= 100) {
       servoDegrees.ground = servoDegrees.ground + 1;
-      groundJointServo.to(servoDegrees.ground, .1);
+      groundJointServo.to(servoDegrees.ground, SERVO_DEGREE_TIME);
     }
 
     if(y < -.8 && servoDegrees.ground >= 0) {
       servoDegrees.ground = servoDegrees.ground - 1;
-      groundJointServo.to(servoDegrees.ground, .1);
+      groundJointServo.to(servoDegrees.ground, SERVO_DEGREE_TIME);
     }
   }
   
   function rotate360DegServo() {
-    const startTime = new Date();
-    
+    if((x > .2 || x < -.2) && middleRotatorStart == 0) {
+      middleRotatorStart = new Date();
+      console.log(middleRotatorStart, 'start')
+    }
+
     if(x > .2) {
-      middleRotatorServo.cw(x);
+      middleRotatorServo.cw(.5);
       servoTurns.middle.direction = 'cw';
     }
 
     if(x < -.2) {
-      middleRotatorServo.ccw(-x);
+      middleRotatorServo.ccw(.5);
       servoTurns.middle.direction = 'ccw';
     }
 
-    if(x < .1 && x > -.1) middleRotatorServo.stop();
-
-    const endTime = new Date();
-
-    servoTurns.middle.time = endTime - startTime;
+    if(x < .1 && x > -.1 && middleRotatorStart != 0) {
+      middleRotatorServo.stop();
+      middleRotatorEnd = new Date();
+      console.log(middleRotatorEnd, 'end')
+      servoTurns.middle.time = middleRotatorEnd - middleRotatorStart;
+      middleRotatorStart = 0;
+      console.log(servoTurns.middle.time, 'time');
+    }
   }
 }
 
@@ -126,15 +147,14 @@ function switchToRotatorServos(controls) {
 }
 
 function switchToMemorizingMovement(
+  servosMemory,
   controls, 
   led, 
   { 
     ground,
     middle
   }, 
-  {
-    middle: middleRotatorServo
-  }
+  rotatorTurns
 ) {
   if(controls.isControllable) {
     controls.isMemorizing = true;
@@ -144,27 +164,116 @@ function switchToMemorizingMovement(
     servosMemory.push({
       groundJointServo: {
         degrees: ground,
-        time: ground * .1
+        time: ground * SERVO_DEGREE_TIME
       },
       middleJointServo: {
         degrees: middle,
-        time: middle * .1
+        time: middle * SERVO_DEGREE_TIME
       },
-      middleRotatorServo
+      middleRotatorServo: {
+        ...rotatorTurns.middle
+      }
     });
+
+    rotatorTurns.middle = {
+      direction: '',
+      time: 0
+    };
+
+    rotatorTurns.ground = {
+      direction: '',
+      time: 0
+    };
   }
 }
 
-function switchToRecreatingMovement(controls, led) {
+function switchToRecreatingMovement(
+  controls, 
+  led, 
+  servosMemory, 
+  middleJointServo,
+  groundJointServo,
+  middleRotatorServo
+) {
   if(!controls.isReplaying) {
     controls.isMemorizing = false;
     controls.isReplaying = true;
     controls.isControllable = false;
+
+    recreateMovement(
+      controls,
+      servosMemory, 
+      middleJointServo, 
+      groundJointServo, 
+      middleRotatorServo,
+      -1
+    );
+
     led.on();
   } else {
     controls.isMemorizing = false;
     controls.isReplaying = false;
     controls.isControllable = true;
     led.off();
+  }
+}
+
+function recreateMovement(
+  controls,
+  servosMemory, 
+  middleJointServo, 
+  groundJointServo, 
+  middleRotatorServo,
+  index
+) {
+  let oldIndex;
+  let timeout = 1000;
+
+  if(index == -1) oldIndex = 0;
+  else oldIndex = index;
+  
+  ++index;
+  if(index === servosMemory.length) index = 0;
+
+  if(
+    servosMemory[oldIndex].groundJointServo.degrees != servosMemory[index].groundJointServo.degrees 
+    || index == 0
+  ) {
+    groundJointServo.to(servosMemory[index].groundJointServo.degrees, SERVO_DEGREE_TIME);
+  }
+
+  if(
+    servosMemory[oldIndex].middleJointServo.degrees != servosMemory[index].middleJointServo.degrees 
+    || index == 0
+  ) {
+    middleJointServo.to(servosMemory[index].middleJointServo.degrees, SERVO_DEGREE_TIME);
+  }
+
+  if(servosMemory[index].middleRotatorServo.time != 0) {
+    if(!timeout || servosMemory[index].middleRotatorServo.time > timeout) {
+      timeout = servosMemory[index].middleRotatorServo.time + 1000;
+    }
+    
+    if(servosMemory[index].middleRotatorServo.direction == 'cw') {
+      middleRotatorServo.cw(.5);
+    } else if(servosMemory[index].middleRotatorServo.direction == 'ccw') {
+      middleRotatorServo.ccw(.5);
+    }
+
+    setTimeout(() => {
+      middleRotatorServo.stop();
+    }, servosMemory[index].middleRotatorServo.time);
+  }
+  
+  if(controls.isReplaying) {
+    setTimeout(() => {
+      recreateMovement(controls,
+        servosMemory, 
+        middleJointServo, 
+        groundJointServo, 
+        middleRotatorServo,
+        index
+      );
+    }, timeout);
   }
 }
